df5c71215ad1702e77f87f6192348610b93e9fbc
mehmeteyupkatirci <mehmeteyupkatirci@gmail.com>
2021-04-10 17:12:30 +0300

    Basic Improvement skills and some gui changes added

--- Pages/GamePage.xaml.cs
+++ Pages/GamePage.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,14 +22,69 @@ namespace Clicker
     /// </summary>
     public partial class GamePage : Page
     {
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private double oneDefaultMoney = 5;
+        private double oneCurrentPrice = 10;
+
         public GamePage()
         {
             InitializeComponent();
+            oneUpgradeCountTxt.Text = 1.ToString();
+            RefreshFirstProgress(_cancellationTokenSource.Token);
+            moneyTxt.Text = 0.ToString();
+            oneMoneyTxt.Text = (Convert.ToDouble(oneUpgradeCountTxt.Text) * oneDefaultMoney).ToString(); 
         }
 
         private void btnFirstPage_Click(object sender, RoutedEventArgs e)
         {
-            this.NavigationService.Navigate(new FirstPage());
+            var messageBoxResult = MessageBox.Show("Anasayfaya dönmek istediğinize emin misiniz?","Idle Clicker", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                this.NavigationService.Navigate(new FirstPage());
+            }            
+        }
+
+        public async void RefreshFirstProgress(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(1000);
+                oneProgBar.Value += 1000;
+                oneMoneyTxt.Text = (Convert.ToDouble(oneUpgradeCountTxt.Text) * oneDefaultMoney).ToString();
+                if (oneProgBar.Value == oneProgBar.Maximum)
+                {
+                    moneyTxt.Text = (Convert.ToDouble(oneMoneyTxt.Text) + Convert.ToDouble(moneyTxt.Text)).ToString();
+                    oneProgBar.Value = 0;
+                }
+            }
+        }
+
+        private void oneProgBar_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (oneProgBar.Value == oneProgBar.Maximum)
+            {
+                moneyTxt.Text = (Convert.ToDouble(oneMoneyTxt.Text) + Convert.ToDouble(moneyTxt.Text)).ToString();
+                oneProgBar.Value = 0;
+            }
+            else
+            {
+                MessageBox.Show("!");
+            }
+        }
+
+        private void oneBuyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToDouble(moneyTxt.Text) >= oneCurrentPrice)
+            {
+                oneUpgradeCountTxt.Text = (Convert.ToDouble(oneUpgradeCountTxt.Text) + 1).ToString();
+                moneyTxt.Text = (Convert.ToDouble(moneyTxt.Text) - oneCurrentPrice).ToString();
+                oneBuyButton.Content = $"${oneCurrentPrice} Buy";
+                oneCurrentPrice = Math.Round(oneDefaultMoney * Math.Pow(1.15, Convert.ToDouble(oneUpgradeCountTxt.Text)), 1);
+            }
+            else
+            {
+                MessageBox.Show("Yetersiz para");
+            }
         }
     }
 }
